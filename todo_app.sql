65bac3eec0303a8350931a8948c46d0705a2439b
gchu <gchu647@gmail.com>
2018-07-30 19:22:28 -1000

    Finished up to 13

--- todo_app.sql
+++ todo_app.sql
@@ -6,10 +6,29 @@ CREATE USER michael ENCRYPTED PASSWORD '1234';
 
 CREATE DATABASE todo_app;
 
+\c todo_app;
+
+CREATE SEQUENCE table_name_id_seq;
+
 CREATE TABLE task (
-  id integer PRIMARY KEY
+  id INTEGER PRIMARY KEY NOT NULL DEFAULT nextval('table_name_id_seq'),
+  title VARCHAR(255),
+  description TEXT,
+  created_at TIMESTAMP NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP,
+  completed boolean NOT NULL DEFAULT FALSE
 );
 
 ALTER TABLE task
-  ADD COLUMN completed_at TYPE timestamp with timezone
-  DROP COLUMN IF EXISTS completed;
+  DROP COLUMN completed,
+  ADD COLUMN completed_at TIMESTAMP DEFAULT NULL,
+  ALTER COLUMN updated_at SET NOT NULL,
+  ALTER COLUMN updated_at SET DEFAULT now();
+
+INSERT INTO task (title, description, created_at, updated_at, completed_at)
+VALUES ('Study SQL', 'Complete this exercise', now(), now(), NULL);
+
+INSERT INTO task(title, description)
+VALUES ('Study PostgreSQL', 'Read all the documentation');
+
+\c george;
