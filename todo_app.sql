90bfa9b51c047c61766b660dd7c763537fb8e498
gchu <gchu647@gmail.com>
2018-07-30 17:11:25 -1000

    Working on create table

--- todo_app.sql
+++ todo_app.sql
@@ -1,4 +1,15 @@
-DROP TABLE IF EXISTS todo_app CASCADE;
+DROP DATABASE IF EXISTS todo_app;
 
 DROP USER IF EXISTS michael;
 
+CREATE USER michael ENCRYPTED PASSWORD '1234';
+
+CREATE DATABASE todo_app;
+
+CREATE TABLE task (
+  id integer PRIMARY KEY
+);
+
+ALTER TABLE task
+  ADD COLUMN completed_at TYPE timestamp with timezone
+  DROP COLUMN IF EXISTS completed;
